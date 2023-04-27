2790db4b5e422496656d7c08d85d34f5ef07bfaa
Hamed Tayebi <hamed-tayebi@outlook.com>
2023-04-27 09:06:30 +0200

    initial commit

---
+++ components/activities-page.js
@@ -0,0 +1,37 @@
+import { data } from "@/lib/db/db";
+
+export default function ActivityCards() {
+  const ActivitiesPage = data.map((element) => {
+    return (
+      <div className="container">
+        <ul key={element.id}>
+          <li
+            style={{
+              fontWeight: "bold",
+              color: "green",
+            }}
+          >
+            Title:
+            {element.title}
+          </li>
+
+          <li>Note: {element.note}</li>
+          <li>You tagged: {element.tags}</li>
+          <img
+            src={element.imageURL}
+            alt="sample image"
+            width="300"
+            height="150"
+          />
+          <li>Date: {element.date}</li>
+          <li>
+            <a href={element.url} target="_blank">
+              your URL
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  });
+  return <div>{ActivitiesPage}</div>;
+}

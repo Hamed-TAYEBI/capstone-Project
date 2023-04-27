8df93679eabeab15ba5804e11becde07e7dfd00d
Hamed Tayebi <hamed-tayebi@outlook.com>
2023-04-27 13:40:03 +0200

    correct key to top level div

--- components/activities-page.js
+++ components/activities-page.js
@@ -1,38 +1,42 @@
 import { data } from "@/lib/db/db";
 import Image from "next/image";
+import Link from "next/link";
 export default function ActivityCards() {
-  const ActivitiesPage = data.map((element) => {
-    return (
-      <div className="container">
-        <ul key={element.id}>
-          <li
-            style={{
-              fontWeight: "bold",
-              color: "green",
-            }}
-          >
-            Title:
-            {element.title}
-          </li>
+  return (
+    <>
+      {data.map((element) => {
+        return (
+          <div className="container" key={element.id}>
+            <ul>
+              <li
+                style={{
+                  fontWeight: "bold",
+                  color: "green",
+                }}
+              >
+                Title:
+                {element.title}
+              </li>
 
-          <li>Note: {element.note}</li>
-          <li>You tagged: {element.tags}</li>
+              <li>Note: {element.note}</li>
+              <li>You tagged: {element.tags}</li>
 
-          <Image
-            src={element.imageURL}
-            alt="sample image"
-            width="300"
-            height="150"
-          />
-          <li>Date: {element.date}</li>
-          <li>
-            <a href={element.url} target="_blank">
-              your URL
-            </a>
-          </li>
-        </ul>
-      </div>
-    );
-  });
-  return <div>{ActivitiesPage}</div>;
+              <Image
+                src={element.imageURL}
+                alt="sample image"
+                width="300"
+                height="150"
+              />
+              <li>Date: {element.date}</li>
+              <li>
+                <Link href={element.url} target="_blank">
+                  your URL
+                </Link>
+              </li>
+            </ul>
+          </div>
+        );
+      })}
+    </>
+  );
 }

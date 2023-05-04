86121f67c3b5387e345b57f05589061ddb1f918b
Hamed Tayebi <hamed-tayebi@outlook.com>
2023-05-04 15:41:05 +0200

    install uuid uuid4

--- components/TagList/TagList.js
+++ components/TagList/TagList.js
@@ -1,3 +1,4 @@
+import { v4 as uuid4 } from "uuid";
 import { tagList } from "@/lib/db/db";
 import Tag from "../Tag";
 export default function TagList({ tags, editTag, activityId, tagId }) {
@@ -7,6 +8,7 @@ export default function TagList({ tags, editTag, activityId, tagId }) {
         const foundTag = tags?.find((id) => tag.id === id);
         return (
           <Tag
+            key={uuid4()}
             activityId={activityId}
             tagId={tagId}
             editTag={editTag}
